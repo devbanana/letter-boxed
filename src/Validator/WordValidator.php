296ae5b90db4f45a30922d54f2b6ee6fd7131d1f
Brandon Olivares <programmer2188@gmail.com>
2023-01-21 20:26:54 -0500

    Create WordValidator to validate given words

---
+++ src/Validator/WordValidator.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devbanana\LetterBoxed\Validator;
+
+final class WordValidator
+{
+    /**
+     * Validate that the given words are valid according to the provided sides.
+     *
+     * @param string[] $words The words to validate
+     * @param string[] $sides The sides containing the 4 sets of letters
+     */
+    public function validate(array $words, array $sides): bool
+    {
+        // Validate that $words contains at least 1 word
+        if (\count($words) < 1) {
+            return false;
+        }
+
+        // Validate that $sides contains exactly 4 elements
+        if (\count($sides) !== 4) {
+            return false;
+        }
+
+        foreach ($words as $i => $word) {
+            if (!$this->validateWord($word, $sides)) {
+                return false;
+            }
+
+            // Validate that the first letter of each word matches the last letter of the prior word
+            if ($i > 0 && !$this->validateWordsMatch($words[$i - 1], $words[$i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates that the given two words are valid according to the provided matches.
+     *
+     * @param string $word1 The first word to validate
+     * @param string $word2 The second word to validate
+     */
+    private function validateWordsMatch(string $word1, string $word2): bool
+    {
+        $lastLetter = strtolower($word1[\strlen($word1) - 1]);
+        $firstLetter = strtolower($word2[0]);
+
+        return $lastLetter === $firstLetter;
+    }
+
+    /**
+     * Validates that the given word is valid according to the provided sides.
+     *
+     * @param string   $word  The word to validate
+     * @param string[] $sides The 4 sides of the puzzle
+     */
+    private function validateWord(string $word, array $sides): bool
+    {
+        if (\strlen($word) < 3) {
+            return false;
+        }
+
+        $previousLetterSide = null;
+
+        foreach (str_split($word) as $letter) {
+            $letterSide = $this->findSideByLetter($letter, $sides);
+            if ($previousLetterSide === $letterSide) {
+                return false;
+            }
+            $previousLetterSide = $letterSide;
+        }
+
+        return true;
+    }
+
+    /**
+     * Find the side that contains the given letter.
+     *
+     * @param string   $letter The letter to search for
+     * @param string[] $sides  The 4 sides of the puzzle
+     */
+    private function findSideByLetter(string $letter, array $sides): ?int
+    {
+        foreach ($sides as $i => $side) {
+            if (stripos($side, $letter) !== false) {
+                return $i;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tests/Validator/WordValidatorTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devbanana\LetterBoxed\Tests\Validator;
+
+use Devbanana\LetterBoxed\Validator\WordValidator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+final class WordValidatorTest extends TestCase
+{
+    private WordValidator $wordValidator;
+
+    /**
+     * @var string[]
+     */
+    private array $sides;
+
+    protected function setUp(): void
+    {
+        $this->wordValidator = new WordValidator();
+        $this->sides = ['XBM', 'INA', 'OYL', 'TEC'];
+    }
+
+    public function testInvalidIfNoWordsGiven(): void
+    {
+        self::assertFalse($this->wordValidator->validate([], $this->sides));
+    }
+
+    public function testConsecutiveLettersFromSameSideAreInvalid(): void
+    {
+        self::assertFalse($this->wordValidator->validate(['tea'], $this->sides));
+        self::assertFalse($this->wordValidator->validate(['intel'], $this->sides));
+    }
+
+    public function testConsecutiveLettersFromDifferentSidesAreValid(): void
+    {
+        self::assertTrue($this->wordValidator->validate(['exclamation'], $this->sides));
+    }
+
+    public function testInvalidIfFirstLetterOfWordDoesNotMatchLastLetterofPreviousWord(): void
+    {
+        self::assertFalse($this->wordValidator->validate(['limitation', 'maybe'], $this->sides));
+    }
+
+    public function testValidIfFirstLetterOfWordMatchesLastLetterOfPreviousWord(): void
+    {
+        self::assertTrue($this->wordValidator->validate(['atom', 'maybe'], $this->sides));
+    }
+}
